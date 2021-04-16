337c5d83d342b57ba6604184a9e4c0fe61c4a877
ammarjama032 <ammarjamal032@gmail.com>
2021-04-17 03:24:06 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Ammar JamaL
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="www.google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ src/Demo.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './Demo.css';
+
+const Demo =() => {
+    return(<div>
+        <div class="head1">
+        <h1> Hello world</h1>
+        <p> Hi this is JAMAL Here... </p>
+        <p>I'm web developer</p>
+        
+        <div class="head2">
+            <p> Hi this is AMMAR JAMAL Here... </p>
+            </div>
+        </div>
+        </div>
+        );
+}
+
+
+export default Demo;
