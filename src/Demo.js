8e815fedfb357dbc33be0390e8333a7047b5dbd2
ammarjama032 <ammarjamal032@gmail.com>
2021-04-24 22:57:36 +0530

    web updated

--- src/Demo.js
+++ src/Demo.js
@@ -10,7 +10,7 @@ const Demo =() => {
             <h1> Welcome to My Page..</h1>
                 <Avtar id='1' name="AMMAR" work="Web Developer"/>
                <div className="button">
-                <a href="https://main.d3q1au4ui2khq.amplifyapp.com/" className="myButton grow">Click Here</a>
+                <a href="https://www.ammarjamal.tech" className="myButton grow">Click Here</a>
                 </div>
             </div>
         )
